77fef605df518de949a0ae325774496cc558e3e1
Abhishek Kumar <rajabhishek7654@gmail.com>
2025-02-27 11:52:40 +0530

    [Abhishek] add. EmployeePayrollApp Adding remaining properties to the Payroll DTO and Model like startDate, note, profilePic and department

--- EmployeePayrollApp/src/main/java/com/bridgelabz/EmployeePayrollApp/dto/EmployeePayrollDTO.java
+++ EmployeePayrollApp/src/main/java/com/bridgelabz/EmployeePayrollApp/dto/EmployeePayrollDTO.java
@@ -5,25 +5,25 @@ import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.Max;
 import lombok.Data;
+import lombok.ToString;
 
-@Data
+import java.time.LocalDate;
+import java.util.List;
+
+@ToString
 public class EmployeePayrollDTO {
 
     @NotEmpty(message = "Name cannot be empty")
     @Pattern(regexp = "^[A-Z][a-zA-Z]{2,}$", message = "Name must start with a capital letter and be at least 3 characters long")
-    private String name;
+    public String name;
 
     @Min(value = 1000, message = "Salary must be at least 1000")
     @Max(value = 1000000, message = "Salary cannot exceed 1,000,000")
-    private int salary;
-    public EmployeePayrollDTO(String name, int salary) {
-        this.name = name;
-        this.salary = salary;
-    }
-
-    @Override
-    public String toString() {
-        return "EmployeePayrollDTO{name='" + name + "', salary=" + salary + "}";
-    }
+    public int salary;
+    public String gender;
+    public LocalDate startDate;
+    public String note;
+    public String profilePic;
+    public List<String> department;
 }
 
